0f2df2cecb4ebff30d7ed0b16929c83399616cf7
ThulaneEjoobi <thulanet@ejoobi.co.za>
2025-07-20 15:15:30 +0200

    Initial commit

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,15 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'mail.squirrelconsulting.co.za',
+    'username' => 'info@squirrelconsulting.co.za',
+    'password' => 'pastor I need the real password',  
     'port' => '587'
+
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
